02892e8f0036ef7d2d205b39e631f1a0376d7393
hemantwagh1221 <hemantwagh074@gmail.com>
2025-10-05 18:06:35 +0530

    may25-3

---
+++ 4_nested_data/none1.py
@@ -0,0 +1,26 @@
+
+
+d1={'tcs':600,'amzn':780,'goog':890}
+print(d1)
+print(d1.get('tcs1',99))
+print(d1['tcs'])
+
+
+a=None
+print(a)
+print(type(a))
+
+a=10
+b=11
+
+#truthy value
+
+
+#falsy value
+# "",0,[],{},(),None
+
+
+if 0:
+    print('inside if')
+else:
+    print('inside else')
