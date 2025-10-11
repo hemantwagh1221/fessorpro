7437d8630fb85812ad690987f259ed75da48e3b4
hemantwagh1221 <hemantwagh074@gmail.com>
2025-10-12 02:13:47 +0530

    may25-7

--- .imp1.py/lib1.py
+++ .imp1.py/lib1.py
@@ -14,3 +14,26 @@
 
 #ugrade a module
 #pip3 install --upgrade module_name
+
+
+
+#prints every sec code 
+def main_strategy():
+    print('running main strategy')
+
+import time
+while True:
+    dt1=dt.datetime.now()
+    print(dt1)
+
+    #every sec
+
+    #every 5 min
+    if dt1.second==1 and dt1.minute in range(0,50,5):
+        main_strategy()
+    #every 1 min
+    if dt1.second==1 :
+        main_strategy()
+
+    
+    time.sleep(1)
---
+++ 12_datetime analysis/date1.py
@@ -0,0 +1,96 @@
+
+data='2024-01-01'
+
+import datetime as dt
+
+#date
+#time
+#datetime
+
+d1=dt.date(2025,1,1)
+print(d1)
+
+t1=dt.time(11,15,15)
+print(t1)
+
+dt1=dt.datetime(2025,7,13,11,15,15)
+print(dt1)
+print(dt1.weekday())
+
+#0 mon
+#1 tue
+#2 wed
+#6 sun
+
+i1=dt.timedelta(minutes=2)
+print(dt1+i1)
+
+#convert datetime to epoch
+print(dt1.timestamp())
+
+#epoch to datetime
+n=1752409073
+a=dt.datetime.fromtimestamp(n)
+print(a)
+
+#datetime to string
+
+# Sunday, 13 July 2025 12:17:53
+
+f='%A, %d %B %Y %H:%M:%S'
+s1=dt1.strftime(f)
+print(s1)
+
+#string to datetime
+s2="Sunday, 13 July 2025 12:17:53"
+f='%A, %d %B %Y %H:%M:%S'
+
+d5=dt.datetime.strptime(s2,f)
+print(d5)
+
+#all thur of year 2025
+start=dt.datetime(2025,1,1)
+for i in range(365):
+    
+    if start.weekday()==3:
+        print(start)
+    start=start+dt.timedelta(days=1)
+
+#creart a fun which shuld giver me current expiry
+def get_nearest_expiry(d1):
+    start=dt.datetime(2025,1,1)
+    for i in range(365):
+    
+        if start.weekday()==3:
+            print(start,(start-d1).days)
+            if (start-d1).days>0:
+                return start
+        start=start+dt.timedelta(days=1)
+
+# d1=dt.datetime.now()
+d1=dt.datetime(2025,12,24)
+print(d1)
+ans=get_nearest_expiry(d1)
+print(ans)
+
+
+
+def main_strategy():
+    print('running main strategy')
+
+import time
+while True:
+    dt1=dt.datetime.now()
+    print(dt1)
+
+    #every sec
+
+    #every 5 min
+    if dt1.second==1 and dt1.minute in range(0,50,5):
+        main_strategy()
+    #every 1 min
+    if dt1.second==1 :
+        main_strategy()
+
+    
+    time.sleep(1)
